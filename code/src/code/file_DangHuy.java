9fe594a272fd852b5ca20d5b82f4e799331a1277
danghuy0604 <huy09051214909@gmail.com>
2022-09-23 22:12:51 +0700

    file cua huy

---
+++ code/src/code/file_DangHuy.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package code;
+
+/**
+ *
+ * @author Dang Huy
+ */
+public class file_DangHuy {
+    /*
+    private void btncongActionPerformed(java.awt.event.ActionEvent evt) {                                        
+        //Double a = Double.parseDouble(edta.getText());
+        //Double b = Double.parseDouble(edtb.getText());
+        String regex = "^[-]?(\\d+(\\.\\d*)?)";
+        String tmpa = edtb.getText();
+        String tmpb = edta.getText();
+        if(tmpa.matches(regex) && tmpb.matches(regex)) { 
+            double a= Double.parseDouble(tmpa);
+            double b= Double.parseDouble(tmpb);
+            double c = a + b;
+            String s = String.valueOf(c);
+            edtkq.setText(s);
+            edtthongbao.setText("");
+        } else {
+            edtthongbao.setText("Đầu vào phải là số");
+        }     
+    }                                       
+
+    private void btntruActionPerformed(java.awt.event.ActionEvent evt) {                                       
+        //Double a = Double.parseDouble(edta.getText());
+        //Double b = Double.parseDouble(edtb.getText());       
+        String regex = "^[-]?(\\d+(\\.\\d*)?)";
+        String tmpa = edtb.getText();
+        String tmpb = edta.getText();
+        if(tmpa.matches(regex) && tmpb.matches(regex)) { 
+            double a= Double.parseDouble(tmpa);
+            double b= Double.parseDouble(tmpb);
+            double c = a - b;
+            String s = String.valueOf(c);
+            edtkq.setText(s);
+            edtthongbao.setText("");
+        } else {
+            edtthongbao.setText("Đầu vào phải là số");
+        }     
+    }               
+    */
+    
+}
